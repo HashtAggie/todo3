6886fffeb89aad8cd94a869b6e934d1a0d12081d
HashtAggie <a.el.arras@gmail.com>
2016-04-22 09:46:47 +0200

    added test gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :todos
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'todos#index'
+  resources :todos, only: [:create, :update, :destroy]
 
-  # You can have the root of your site routed with "root"
-root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
